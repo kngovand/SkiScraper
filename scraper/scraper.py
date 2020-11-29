ebeaf1c7ed2cba112e26c63dbcabd908e00c4582
K Duc <kngovand@gmail.com>
2020-11-28 20:00:25 -0700

    Scraping complete with xpath, finding element by id/class is tedious

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 # Scraper to grab Winterpark.com data, unsuccessful due to Incapsula error - Using Selenium 
 # Side note: Cannot grab HTML but can find elements
+
 from bs4 import element
 import requests
 import time
@@ -17,5 +18,19 @@ url = 'https://www.winterparkresort.com/the-mountain/mountain-report#/'
 driver = webdriver.Chrome(options=options, executable_path="c:\\chromedriver.exe")
 driver.get(url)
 
-l = driver.find_element_by_class_name('temp-today').text
-print(l) 
+# Grabbing data via Xpath - selecting by id/class is tedious
+temp = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[1]/div/div[3]/div[2]/div[1]/div[1]/div[4]/div[1]/span').text
+depth_total = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/span[1]').text
+depth_overnight = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]').text
+depth_24 = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div[4]/div[2]').text
+depth_48 = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div[5]/div[2]').text
+lifts = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[2]/div/div[1]/div[2]').text
+trails = driver.find_element_by_xpath('//*[@id="mountainConditionsApp"]/div[2]/div[2]/div/div[2]/div[2]').text
+
+print(temp + ' F')
+print('Total: ' + depth_total)
+print('Overnight: ' + depth_overnight)
+print('24 hrs: ' + depth_24)
+print('48 hrs: ' + depth_48)
+print('Lifts open: ' + lifts)
+print('Trails open: ' + trails)
