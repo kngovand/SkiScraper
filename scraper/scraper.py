f810074b84da7ec1ed71ed30eb6691de1a197754
K Duc <kngovand@gmail.com>
2020-11-28 17:21:41 -0700

    Web request working, now starting on HTML parsing

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,12 @@
-print("hello")
+import requests
+
+# Go to Winter Park mountain stats webpage
+url = "https://www.winterparkresort.com/the-mountain/mountain-report"
+x = requests.get(url)
+
+# If python requests didn't work
+if(x.status_code != 200):
+    print("Invalid status code")
+else:
+    print(x.headers)
+    print(x.content)
