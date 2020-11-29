5dad0b21e44076d888fc36f64b9e2f2dc1ecbe00
K Duc <kngovand@gmail.com>
2020-11-28 19:05:53 -0700

    Selenium now grabbing data!

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,21 @@
+# Scraper to grab Winterpark.com data, unsuccessful due to Incapsula error - Using Selenium 
+# Side note: Cannot grab HTML but can find elements
+from bs4 import element
 import requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# Go to Winter Park mountain stats webpage
-url = "https://www.winterparkresort.com/the-mountain/mountain-report"
-x = requests.get(url)
+# Setting up Selenium
+options = Options()
+options.add_argument("--window-size=1920,1200")
+# Run webdriver without browser
+options.headless = True
 
-# If python requests didn't work
-if(x.status_code != 200):
-    print("Invalid status code")
-else:
-    print(x.headers)
-    print(x.content)
+url = 'https://www.winterparkresort.com/the-mountain/mountain-report#/'
+driver = webdriver.Chrome(options=options, executable_path="c:\\chromedriver.exe")
+driver.get(url)
+
+l = driver.find_element_by_class_name('temp-today').text
+print(l) 
